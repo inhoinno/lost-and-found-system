206ca3ab450f33c40b76fc49daafac3d8d84baf8
Inho Song <mearrong123@gmail.com>
2020-11-07 00:15:00 +0900

    WiFi + Sensor + Raspberry : Lost_and_Foundv0.1

--- Arduino_WEBMOS_D1W1/Lost_and_foud_arduino_ESP13/Lost_and_foud_arduino_ESP13.ino
+++ Arduino_WEBMOS_D1W1/Lost_and_foud_arduino_ESP13/Lost_and_foud_arduino_ESP13.ino
@@ -4,7 +4,7 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #ifndef STASSID
-#define STASSID "raspi_SSongJeong"
+#define STASSID "raspi_inhoinno"
 #define STAPSK  "32152332"
 #endif
 
@@ -19,77 +19,32 @@ char host [] = "192.168.4.1";
 //Laser & CDS setup
 int cds = A0;
 const int LaserPin = 13;
-int sensor = 2;
+int LED = 9;
 int count =0;
 WebSocketClient webSocketClient;
 WiFiClient client;
-void _WiFi_setup(){
-   WiFi.mode(WIFI_STA);
-  s_status = WiFi.begin(ssid, password);
-  Serial.println("");
-Serial.println("  WiFi Begin() Done.");
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-   //s_status =  WiFi.begin(ssid, password);
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("You're connect to the network \"");
-  Serial.print(ssid);
-  Serial.println("\"");
-
- 
- // 서버에 연결
-  /**************************************/
-  /*****  Server Connection Setup  *****/
-
-    if (client.connect(host, Port)) {
-        Serial.println("Connected");
-    } else {
-        Serial.println("Connection failed.");
-    }
-    delay(1000); 
-
-    webSocketClient.path = path;
-    webSocketClient.host = host;
- 
-    if (webSocketClient.handshake(client)) {
-        Serial.println("Handshake successful");
-    } else {
-        Serial.println("Handshake failed.");
-    }
-      
-  }
 void setup() {
   // put your setup code here, to run once:  
   Serial.begin(115200);
-  Serial.println("Laser Setup Start...");
+  
   /**************************************/
   /***********Laser Sensor Set***********/
   
   pinMode(cds,INPUT); //CDS : 조도센서
-  pinMode(sensor, OUTPUT); //LED : 감지 ON/OFF 
+  pinMode(LED, OUTPUT); //LED : 감지 ON/OFF 
   pinMode(LaserPin, OUTPUT); // Laser Module 
   pinMode(LED_BUILTIN, OUTPUT); //WIFI LED
   
   /*********Laser Sensor END************/
   /**************************************/
-Serial.println("Laser Setup Done.");
+
   /**************************************/
   /**********   WiFi Setup      *********/
-  Serial.println("WiFi Setup Start...");
-  Serial.println("  WiFi Begin() Start...");
-    // ERROR : ets Jan  8 2013,rst cause:4, boot mode:(3,6)
+
   WiFi.mode(WIFI_STA);
   s_status = WiFi.begin(ssid, password);
   Serial.println("");
-Serial.println("  WiFi Begin() Done.");
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
@@ -135,41 +90,36 @@ Serial.println("  WiFi Begin() Done.");
 
 
 void loop() {
-   delay(1000);
+    delay(1000);
   String data = "Sensor 1";  
 
   // put your main code here, to run repeatedly:
   digitalWrite(LaserPin, HIGH); 
-     // Turn the LED on (Note that LOW is the voltage level
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
   cds = analogRead(A0);
   Serial.println(cds) ;
-  if(cds < 800){
-    digitalWrite(sensor,HIGH);
-    digitalWrite(LED_BUILTIN, HIGH);}
-  else {digitalWrite(sensor, LOW);
-  digitalWrite(LED_BUILTIN, LOW);}
-
+  if(cds < 700){digitalWrite(LED,HIGH);}
+  else {digitalWrite(LED, LOW);}
+  delay(500);
   // but actually the LED is on; this is because
   // it is active low on the ESP-01)
-
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  
 
   if ((client.connected())){
     //연결 완료
     //데이터 전송
     webSocketClient.sendData("msg count : "+String(count++));
-    Serial.println(String(count)+"Send.");
-    //webSocketClient.getData(data);
-    //if(data.length() > 0)
-      //Serial.println(data);
+    //Serial.print("Sensor 1\n");
+    webSocketClient.getData(data);
+    if(data.length() > 0)
+      Serial.println(data);
 
     }else{
-       Serial.print(".");
-       _WiFi_setup();  //A Line to avoid webSocket Error 1006 : "connection failed with no reason"
-      //When Connection is Failed Due to websocket Network error, Try WiFi.begin again 
+          Serial.print(".");
               return;
      }
 
-    //webSocketClient.sendData("msg count : "+String(count++));
     // capture the value of analog 1, send it along
     //pinMode(1, INPUT);
     //data = String(analogRead(1));
--- Rasberrypi4_Server/server.py
+++ Rasberrypi4_Server/server.py
@@ -4,25 +4,32 @@ import fcntl
 import struct
 import asyncio
 import websockets
-
+import time
 def get_ipaddress(network):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return socket.inet_ntoa(fcntl.ioctl(
         s.fileno(),
         0x8915, #SIOCGIFADDR
-        struct.pack('256s',network[:15].encode('utf-8'))
+        struct.pack('256s',network[:24].encode('utf-8'))
         )[20:24])
 
 
 async def echo(websocket, path):
-    async for message in websocket :
-        print(message)
-        await websocket.send(message)
+    try:
+        async for message in websocket :
+            print(message)
+            time.sleep(1)
+    except:
+        print(".");
+        pass
+
+        #await websocket.send(message)
 
 
 print("Server start  :" + get_ipaddress('wlan0'))
 
-port = 80
+port = 9999 
 asyncio.get_event_loop().run_until_complete(
-        websockets.serve(echo,get_ipaddress('wlan0'), port))
+    websockets.serve(echo,get_ipaddress('wlan0'), port))
 asyncio.get_event_loop().run_forever()
+
